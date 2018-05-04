ad784258f17c1987f4d1d78d37f23a853eb73711
Marco Guidolin <m.guidolin@egeatech.com>
2018-05-04 16:32:33 +0200

    ADD: hooks for annotation manipulations to be sent to server and support for annotation deletion and edit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import twitter from 'twitter-text';
 import PDFJSAnnotate from '../';
 import initColorPicker from '../shared/initColorPicker';
-import sync from './utils/syncAnnotations';
+import { sync, broadcastVariations} from './utils/syncAnnotations';
+import { addEventListener } from './UI/event';
 
 const { UI } = PDFJSAnnotate;
 const documentId = 'example.pdf';
@@ -359,9 +360,68 @@ render();
   UI.addEventListener('annotation:blur', handleAnnotationBlur);
 })(window, document);
 
+// Socket server sync listeners
+addEventListener('annotation:add', (...args) => {
+  console.log('Added annotation');
+
+  const socketObj = {
+    syncType: 'add',
+    class: 'annotation',
+    data: args,
+  };
+  broadcastVariations(socketObj);
+});
+
+addEventListener('annotation:edit', (...args) => {
+  console.log('Edited annotation');
+
+  const socketObj = {
+    syncType: 'edit',
+    class: 'annotation',
+    data: args,
+  };
+  broadcastVariations(socketObj);
+});
+
+addEventListener('annotation:delete', (...args) => {
+  console.log('Deleted annotation');
+
+  const socketObj = {
+    syncType: 'delete',
+    class: 'annotation',
+    data: args,
+  };
+  broadcastVariations(socketObj);
+});
+
+addEventListener('comment:add', (...args) => {
+  console.log('Added comment to annotation');
+
+  const socketObj = {
+    syncType: 'add',
+    class: 'comment',
+    data: args,
+  };
+  broadcastVariations(socketObj);
+});
+
+// FIXME: emitted by no one, yet
+addEventListener('comment:delete', (...args) => {
+  console.log('Deleted comment');
+
+  const socketObj = {
+    syncType: 'add',
+    class: 'annotation',
+    data: args,
+  };
+  broadcastVariations(socketObj);
+});
+
+// TEST stuff:
+
 document.addEventListener('test', (e) => {
   console.log('Intercepted sync event trigger');
-  sync(e.detail, RENDER_OPTIONS);
+  sync(e.detail, RENDER_OPTIONS, true);
 });
 
 /*
@@ -370,13 +430,4 @@ document.addEventListener('test', (e) => {
  * var obj = JSON.parse('[{"type":"strikeout","color":"FF0000","rectangles":[{"y":351.984375,"x":343.30810546875,"width":212.34393310546875,"height":10},{"y":361.9375,"x":317,"width":239.114501953125,"height":8},{"y":361.9375,"x":317,"width":239.09930419921875,"height":10},{"y":371.90625,"x":317,"width":239.118896484375,"height":8},{"y":371.90625,"x":317,"width":239.10498046875,"height":10},{"y":381.859375,"x":317,"width":221.77508544921875,"height":8},{"y":381.859375,"x":317,"width":221.76092529296875,"height":10},{"y":391.828125,"x":328.96875,"width":227.14947509765625,"height":8},{"y":391.828125,"x":328.96875,"width":227.13482666015625,"height":10},{"y":401.796875,"x":317,"width":124.9637451171875,"height":10}],"class":"Annotation","uuid":"67c7aa11-096b-4b46-baf0-02ce275c3cc1","page":1},{"type":"strikeout","color":"FF0000","rectangles":[{"y":351.984375,"x":343.30810546875,"width":212.34393310546875,"height":10},{"y":361.9375,"x":317,"width":239.114501953125,"height":8},{"y":361.9375,"x":317,"width":239.09930419921875,"height":10},{"y":371.90625,"x":317,"width":239.118896484375,"height":8},{"y":371.90625,"x":317,"width":239.10498046875,"height":10},{"y":381.859375,"x":317,"width":221.77508544921875,"height":8},{"y":381.859375,"x":317,"width":221.76092529296875,"height":10},{"y":391.828125,"x":328.96875,"width":227.14947509765625,"height":8},{"y":391.828125,"x":328.96875,"width":227.13482666015625,"height":10},{"y":401.796875,"x":317,"width":124.9637451171875,"height":10}],"class":"Annotation","uuid":"ea298afe-3aff-44b0-b0ab-31d619b98f75","page":1},{"type":"area","x":309,"y":412,"width":248,"height":129,"class":"Annotation","uuid":"c827fedb-03d3-479b-a943-943c87bb7426","page":1},{"class":"Comment","uuid":"0e3b880b-01ac-4d06-ba71-f27bef9cfa0a","annotation":"c827fedb-03d3-479b-a943-943c87bb7426","content":"bduibcduil"}]');
  * document.dispatchEvent(new CustomEvent('test', { detail: obj }));
  *
- */
-
-/*
- * TODO: hook socket connection to these events:
- * • annotation:add
- * • annotation:edit
- * • annotation:delete
- * • comment:add
- * • comment:delete
  */
--- src/utils/syncAnnotations.js
+++ src/utils/syncAnnotations.js
@@ -10,25 +10,60 @@ let RENDER_OPTIONS = {};
  * @return {*}
  */
 function diff(oldAnnotations, newAnnotations) {
-  if (!oldAnnotations || !newAnnotations) {
-    return !oldAnnotations ? newAnnotations : [];
+  if (!newAnnotations || (!newAnnotations && !oldAnnotations)) {
+    return [];
+  } else if (!oldAnnotations) {
+    return newAnnotations
+      .filter(item => item.syncType === 'add')
+      .map(i => i.data[2])
   }
 
+  let annotationBuffer = oldAnnotations;
   const changed = [];
-  // FIXME: probably unnecessary loop
+
   newAnnotations
     .forEach(i => {
-      const idx = oldAnnotations.findIndex(el => i.uuid === el.uuid);
-      if (idx >= -1) {
-        changed.push(i);
+
+      const idx = annotationBuffer
+        .map((el) => el.uuid)
+        .indexOf(i.data[1]);
+
+      switch (i.syncType) {
+        case 'add':
+          changed.push(i.data[2]);
+          break;
+        case 'edit':
+          changed.push(i.data[2]);
+          // Removing outdated annotation
+          annotationBuffer.splice(idx, 1);
+          break;
+        case 'delete':
+          // Removing annotation
+          annotationBuffer.splice(idx, 1);
+          // Removing comments linked to deleted annotation
+          annotationBuffer = annotationBuffer.filter(item => typeof item.annotation === "undefined" || item.annotation !== i.data[1]);
+          break;
       }
     });
 
-  return changed;
+  return changed.concat(annotationBuffer);
 }
 
 /**
- * Method to update the data in localstorage with the new annotations
+ * Utility function to extract page numbers from an annotations array
+ *
+ * @param annotations
+ * @return {*}
+ */
+function extractPageNumbers(annotations) {
+  return annotations
+    .map(i => typeof i.page !== "undefined" ? i.page : -1)
+    .sort()
+    .filter((item, idx, arr) => (item !== -1) && (!idx || item !== arr[idx - 1]));
+}
+
+/**
+ * Method to update the data in localStorage with the new annotations
  *
  * @param newAnnotations
  * @param isFirstSync
@@ -38,23 +73,23 @@ function updateAnnotations(newAnnotations, isFirstSync) {
   return new Promise((resolve, reject) => {
     try {
       // Read from localStorage
-      let storedItems = [];
-      if (!isFirstSync && localStorage.getItem(`${RENDER_OPTIONS.documentId}/annotations`).length) {
-        storedItems = JSON.parse(localStorage.getItem(`${RENDER_OPTIONS.documentId}/annotations`));
+      let pagesToBeReloaded = [];
+      let storedItems = JSON.parse(localStorage.getItem(`${RENDER_OPTIONS.documentId}/annotations`));
+      if (isFirstSync || typeof storedItems.length === "undefined") {
+        if (typeof storedItems.length !== "undefined" && storedItems.length > 0) {
+          pagesToBeReloaded = extractPageNumbers(storedItems);
+        }
+        storedItems = [];
       }
 
       // Fetch elements to be added or updated
-      const changed = diff(storedItems, newAnnotations);
+      const updatedAnnotations = diff(storedItems, newAnnotations);
 
       // Write new data to localStorage
-      storedItems = changed;
-      localStorage.setItem(`${RENDER_OPTIONS.documentId}/annotations`, JSON.stringify(storedItems));
+      localStorage.setItem(`${RENDER_OPTIONS.documentId}/annotations`, JSON.stringify(updatedAnnotations));
 
-      // Extract pages to be re-rendered
-      const pagesToBeReloaded = changed
-        .map(i => typeof i.page !== "undefined" ? i.page : -1)
-        .sort()
-        .filter((item, idx, arr) => (item !== -1) && (!idx || item !== arr[idx - 1]));
+      // Extract pages to be re-rendered only if it is not the first sync
+      pagesToBeReloaded = extractPageNumbers(updatedAnnotations).concat(pagesToBeReloaded);
 
       resolve(pagesToBeReloaded);
     } catch (e) {
@@ -67,9 +102,9 @@ function updateAnnotations(newAnnotations, isFirstSync) {
  * Reloads the pages of the PDF whose annotations changed
  *
  * @param pages
+ * @return {Promise<any>}
  */
 function reloadPages(pages) {
-  console.log('Updating pages: ', pages);
   return new Promise((resolve, reject) => {
     const promises = [];
     pages.forEach(page => {
@@ -88,7 +123,7 @@ function reloadPages(pages) {
  * @param renderOptions
  * @param isFirstSync
  */
-export default function sync(newAnnotations, renderOptions, isFirstSync) {
+export function sync(newAnnotations, renderOptions, isFirstSync) {
   // Exposes current PDF config settings
   RENDER_OPTIONS = renderOptions;
 
@@ -97,4 +132,13 @@ export default function sync(newAnnotations, renderOptions, isFirstSync) {
     .catch(err => {
       console.log('An error occurred: ', err);
     });
+}
+
+/**
+ * Broadcast to the socket server the variations in the annotations of current document
+ *
+ * @param variationsObj
+ */
+export function broadcastVariations(variationsObj) {
+  // TODO: send variationsObj to server, which will broadcast to all clients inside the related "room"
 }
